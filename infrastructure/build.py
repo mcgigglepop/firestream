dc0cd48dc594981e2b270b81f8c52d968143dc3f
cristopher ontiveros <christoph.alano@gmail.com>
2025-03-04 04:31:59 -0600

    added the infrastructure cdk

---
+++ infrastructure/build.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import sys
+import shutil
+
+
+def exit_on_failure(exit_code, msg):
+    if exit_code != 0:
+        print(msg)
+        exit(exit_code)
+
+
+npm_cmd = shutil.which("npm")
+npx_cmd = shutil.which("npx")
+
+cmd = [npm_cmd, "install"]
+proc = subprocess.run(cmd, stderr=subprocess.STDOUT, shell=False)
+exit_on_failure(proc.returncode, "Cdk npm install failed")
+
+# cmd = [npx_cmd, "cdk", "synth"]
+# proc = subprocess.run(cmd, stderr=subprocess.STDOUT, shell=False)
+# exit_on_failure(proc.returncode, "Cdk synth failed")
---
+++ infrastructure/lib/pipeline-stack.ts
@@ -0,0 +1,111 @@
+import * as cdk from "aws-cdk-lib";
+import * as codecommit from "aws-cdk-lib/aws-codecommit";
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
+import * as cdkPipelines from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+import { GameAnalyticsPipelineConfig } from "./helpers/config-types";
+import { InfrastructureStack } from "./infrastructure-stack";
+
+/* eslint-disable @typescript-eslint/no-empty-interface */
+export interface PipelineStackProps extends cdk.StackProps {
+  config: GameAnalyticsPipelineConfig;
+}
+
+const defaultProps: Partial<PipelineStackProps> = {};
+
+/**
+ * Deploys the Pipeline Stack
+ */
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: PipelineStackProps) {
+    super(scope, id, props);
+
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    props = { ...defaultProps, ...props };
+
+    // Create default repo, will start empty and fail, need to commit this repo to kick off a successful pipeline
+    const sourceRepo = new codecommit.Repository(this, "SourceRepository", {
+      repositoryName: props.config.CODECOMMIT_REPO,
+      description: `Source Code Repository for the ${props.config.WORKLOAD_NAME}`,
+    });
+
+    const pipelineSource = cdkPipelines.CodePipelineSource.codeCommit(
+      sourceRepo,
+      "main"
+    );
+
+    // Buildpsec used for synth
+    const buildSpec = {
+      phases: {
+        install: {
+          "runtime-version": {
+            nodejs: props.config.NODE_VERSION,
+            python: props.config.PYTHON_VERSION,
+          },
+        },
+      },
+    };
+
+    // Synth step runs npm steps to get proper output needed by cdk pipelines
+    const synth = new cdkPipelines.CodeBuildStep("Synth", {
+      input: pipelineSource,
+      partialBuildSpec: codebuild.BuildSpec.fromObject(buildSpec),
+      commands: ["npm run build"],
+      primaryOutputDirectory: "infrastructure/cdk.out",
+    });
+
+    // Deployment pipeline
+    const pipeline = new cdkPipelines.CodePipeline(this, "Pipeline", {
+      synth,
+      // cliVersion: props.config.CDK_VERSION,
+      crossAccountKeys: true,
+      dockerEnabledForSynth: true,
+    });
+
+    // Creates a deployment stage for each stage in config
+    props.config.accounts.forEach(({ NAME, ACCOUNT, REGION }) => {
+      this.addStage(pipeline, NAME, ACCOUNT, REGION, props.config);
+    });
+  }
+
+  // Add a deployment stage
+  addStage(
+    pipeline: cdkPipelines.CodePipeline,
+    stageName: string,
+    stageAccount: string,
+    stageRegion: string,
+    config: GameAnalyticsPipelineConfig
+  ) {
+    const stageConstruct = new cdk.Stage(this, stageName, {
+      env: {
+        account: stageAccount,
+        region: stageRegion,
+      },
+    });
+
+    const deploymentStep = new InfrastructureStack(
+      stageConstruct,
+      config.WORKLOAD_NAME,
+      {
+        config,
+      }
+    );
+
+    // If QA we will add an approval stage after
+    if (stageName === "QA") {
+      const manualApprovalStep = new cdkPipelines.ManualApprovalStep(
+        "ProductionApproval",
+        {
+          comment:
+            "Reviewed Test Results and Approve/Reject for Production Deployment?",
+        }
+      );
+
+      pipeline.addStage(stageConstruct, {
+        post: [manualApprovalStep],
+      });
+    } else {
+      pipeline.addStage(stageConstruct);
+    }
+  }
+}
